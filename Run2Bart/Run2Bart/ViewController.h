57a3979af2d1d0630c3bc2129a8af8ac148711ef
Pete Hodgson <git@thepete.net>
2013-03-28 10:44:27 -0700

    initial commit

---
+++ Run2Bart/Run2Bart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Run2Bart
+//
+//  Created by Pete Hodgson on 3/28/13.
+//  Copyright (c) 2013 ThoughtWorks. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Run2Bart/Run2BartTests/Run2BartTests.h
@@ -0,0 +1,13 @@
+//
+//  Run2BartTests.h
+//  Run2BartTests
+//
+//  Created by Pete Hodgson on 3/28/13.
+//  Copyright (c) 2013 ThoughtWorks. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Run2BartTests : SenTestCase
+
+@end
