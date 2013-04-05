5e659570aa69617c0d83d61e787b2f23a0c0f241
Pete Hodgson <git@thepete.net>
2013-04-05 16:09:29 -0700

    comment out all existing tests

--- Run2Bart/Run2BartTests/MyFirstSpec.m
+++ Run2Bart/Run2BartTests/MyFirstSpec.m
@@ -1,10 +1,10 @@
-#import "Kiwi.h"
+//#import "Kiwi.h"
 
-SPEC_BEGIN(MyFirstSpec)
-describe(@"Basic Arithmetic", ^{
-    specify(^{
-        [[theValue(2+2) should] equal:theValue(4)];
-    });
-});
+//SPEC_BEGIN(MyFirstSpec)
+//describe(@"Basic Arithmetic", ^{
+//    specify(^{
+//        [[theValue(2+2) should] equal:theValue(4)];
+//    });
+//});
 
-SPEC_END
+//SPEC_END
--- Run2Bart/Run2BartTests/StationSpec.m
+++ Run2Bart/Run2BartTests/StationSpec.m
@@ -1,24 +1,24 @@
-#import "Kiwi.h"
+//#import "Kiwi.h"
 
-#import "Station.h"
+//#import "Station.h"
 
-SPEC_BEGIN(StationSpec)
-describe(@"Station", ^{
+//SPEC_BEGIN(StationSpec)
+//describe(@"Station", ^{
     
-    it(@"loads from JSON correctly",^{
-        NSArray *json = @[
-            @{@"abbr":@"s1",@"name":@"station one"},
-            @{@"abbr":@"s2",@"name":@"station two"},
-        ];
-        NSArray *stations = [Station loadStations:json];
-        
-        [[theValue(stations.count) should] equal:theValue(2)];
-        
-        [[[stations[0] abbr] should] equal:@"s1"];
-        [[[stations[0] name] should] equal:@"station one"];
-        [[[stations[1] abbr] should] equal:@"s2"];
-        [[[stations[1] name] should] equal:@"station two"];
-    });
-});
+//    it(@"loads from JSON correctly",^{
+//        NSArray *json = @[
+//            @{@"abbr":@"s1",@"name":@"station one"},
+//            @{@"abbr":@"s2",@"name":@"station two"},
+//        ];
+//        NSArray *stations = [Station loadStations:json];
+//        
+//        [[theValue(stations.count) should] equal:theValue(2)];
+//        
+//        [[[stations[0] abbr] should] equal:@"s1"];
+//        [[[stations[0] name] should] equal:@"station one"];
+//        [[[stations[1] abbr] should] equal:@"s2"];
+//        [[[stations[1] name] should] equal:@"station two"];
+//    });
+//});
 
-SPEC_END
+//SPEC_END
