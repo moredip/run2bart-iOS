e8aa33c8e2d1508515aacdb1facde29858bc2d15
Pete Hodgson <git@thepete.net>
2013-04-09 10:25:31 -0700

    reformat

--- Run2Bart/Run2BartTests/StationsViewControllerSpec.m
+++ Run2Bart/Run2BartTests/StationsViewControllerSpec.m
@@ -9,9 +9,9 @@ describe(@"StationsViewController", ^{
     describe( @"table rendering", ^{
         it(@"renders the right number of rows",^{
             NSArray *stations = @[
-                [[Station alloc] initWithName:@"station one" abbr:@"s1"],
-                [[Station alloc] initWithName:@"station two" abbr:@"s2"],
-                [[Station alloc] initWithName:@"station three" abbr:@"s3"]
+            [[Station alloc] initWithName:@"station one" abbr:@"s1"],
+            [[Station alloc] initWithName:@"station two" abbr:@"s2"],
+            [[Station alloc] initWithName:@"station three" abbr:@"s3"]
             ];
             StationsViewController *stationsVC = [[StationsViewController alloc] init];
             stationsVC.stations = stations;
@@ -19,7 +19,7 @@ describe(@"StationsViewController", ^{
             [[theValue([stationsVC numberOfSectionsInTableView:nil]) should] equal:theValue(1)];
             [[theValue([stationsVC tableView:nil numberOfRowsInSection:0]) should] equal:theValue(3)];
         });
-
+        
         it(@"renders table view cells correctly",^{
             NSArray *stations = @[
             [[Station alloc] initWithName:@"station one" abbr:@"s1"],
@@ -30,7 +30,7 @@ describe(@"StationsViewController", ^{
             stationsVC.stations = stations;
             
             UITableViewCell *secondTableViewCell = [stationsVC tableView:nil cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
-
+            
             [[secondTableViewCell.textLabel.text should] equal:@"station two"];
         });
         
--- Run2Bart/Run2BartTests/UpcomingDeparturesViewControllerSpec.m
+++ Run2Bart/Run2BartTests/UpcomingDeparturesViewControllerSpec.m
@@ -14,8 +14,8 @@ describe( @"UpcomingDeparturesViewController ", ^{
         station = [[Station alloc] initWithName:@"station name" abbr:@"station-abbr"];
         departuresVC = [[UpcomingDeparturesViewController alloc] initForStation:station];
     });
-
-    describe(@"loading upcoming departures", ^{        
+    
+    describe(@"loading upcoming departures", ^{
         it( @"asks the api client to load the station's departures", ^{
             id mockBartClient = [KWMock mockForClass:[BartClient class]];
             departuresVC.bartClient = mockBartClient;
@@ -28,21 +28,21 @@ describe( @"UpcomingDeparturesViewController ", ^{
             [departuresVC viewWillAppear:YES];
         });
     });
-
+    
     describe(@"rendering departures", ^{
         it( @"renders the destination name", ^{
             NSArray *departures = @[
-                [[UpcomingDeparture alloc] initWithDestinationName:@"dest 1" etd:@(0)],
-                [[UpcomingDeparture alloc] initWithDestinationName:@"dest 2" etd:@(1)],
-                [[UpcomingDeparture alloc] initWithDestinationName:@"dest 3" etd:@(2)]
+            [[UpcomingDeparture alloc] initWithDestinationName:@"dest 1" etd:@(0)],
+            [[UpcomingDeparture alloc] initWithDestinationName:@"dest 2" etd:@(1)],
+            [[UpcomingDeparture alloc] initWithDestinationName:@"dest 3" etd:@(2)]
             ];
             departuresVC.departures = departures;
             
             UITableViewCell *tableViewCell = [departuresVC tableView:nil
-                                      cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
+                                               cellForRowAtIndexPath:[NSIndexPath indexPathForRow:1 inSection:0]];
             [[tableViewCell.textLabel.text should] equal:@"dest 2"];
         });
-
+        
         it( @"renders the etd", ^{
             NSArray *departures = @[
             [[UpcomingDeparture alloc] initWithDestinationName:@"dest 1" etd:@(0)],
