2bcd3080c4fdbbe8b59dff4a2c53d13e77da3c35
Pete Hodgson <git@thepete.net>
2013-04-21 15:39:19 -0700

    try using sudo to run homebrew

--- travis/script.sh
+++ travis/script.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-brew install ios-sim
-IOS_SIM_DIR=`brew info ios-sim | sed -n '3 p' | awk '{print $1}'`
-cp $IOS_SIM_DIR/bin/ios-sim ./
-ls -l .
-
-xcodebuild -list -workspace Run2Bart/Run2Bart.xcworkspace
+#export PATH="`pwd`/bin:$PATH"
+#echo "PATH: $PATH"
 
 rake ci
