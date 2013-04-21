82ccbf7a20bdf1181976838067015fc62816449a
Pete Hodgson <git@thepete.net>
2013-04-21 15:42:20 -0700

    nope, Travis setup does not want sudo brew install

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -1,8 +1,8 @@
 xcodebuild -list -workspace Run2Bart/Run2Bart.xcworkspace
 
-sudo brew install ios-sim
-#IOS_SIM_DIR=`brew info ios-sim | sed -n '3 p' | awk '{print $1}'`
-#mkdir bin
-#cp $IOS_SIM_DIR/bin/ios-sim ./bin/ios-sim
+brew install ios-sim
+IOS_SIM_DIR=`brew info ios-sim | sed -n '3 p' | awk '{print $1}'`
+mkdir bin
+cp $IOS_SIM_DIR/bin/ios-sim ./bin/ios-sim
 
-#export PATH="`pwd`/bin:$PATH"
+export PATH="`pwd`/bin:$PATH"
