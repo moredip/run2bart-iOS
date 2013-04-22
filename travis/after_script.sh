334d36241a4773a3d90252f19a055b2002ae4b62
Pete Hodgson <git@thepete.net>
2013-04-22 16:44:31 -0700

    export travis env var. Duh.

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-screencapture $TRAVIS_ARTIFACT_DIR/after_script.jpg
+screencapture $TRAVIS_ARTIFACT_DIR/after_script_a.jpg
 
 rake ci:stop-recording
 rake ci:save-recording["full-travis-run"]
@@ -8,5 +8,7 @@ rake ci:save-recording["full-travis-run"]
 sleep 10
 
 mv /tmp/screenies $TRAVIS_ARTIFACT_DIR/screenies
+
+screencapture $TRAVIS_ARTIFACT_DIR/after_script_b.jpg
 
 travis-artifacts upload --path $TRAVIS_ARTIFACT_DIR --target-path $TRAVIS_BUILD_ID/$TRAVIS_JOB_ID
--- travis/before_script.sh
+++ travis/before_script.sh
@@ -6,7 +6,7 @@ mkdir bin
 cp $IOS_SIM_DIR/bin/ios-sim ./bin/ios-sim
 
 gem install travis-artifacts
-TRAVIS_ARTIFACT_DIR=`pwd`/artifacts
+export TRAVIS_ARTIFACT_DIR=`pwd`/artifacts
 mkdir -p $TRAVIS_ARTIFACT_DIR
 
 rake ci:start-recording
