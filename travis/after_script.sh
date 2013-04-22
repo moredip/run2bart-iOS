a16388dd202a56f6a016cb5637f1a930d9e64749
Pete Hodgson <git@thepete.net>
2013-04-22 16:36:02 -0700

    first take at storing travis artifacts

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-screencapture after_script.jpg
+screencapture $TRAVIS_ARTIFACT_DIR/after_script.jpg
 
 rake ci:stop-recording
-rake ci:save-recording["travis-build-$TRAVIS_BUILD_NUMBER"]
+rake ci:save-recording["full-travis-run"]
 
 sleep 10
 
-ls -l /tmp/screenies/
+mv /tmp/screenies $TRAVIS_ARTIFACT_DIR/screenies
+
+travis-artifacts upload --path $TRAVIS_ARTIFACT_DIR --target-path $TRAVIS_BUILD_ID/$TRAVIS_JOB_ID
--- travis/before_script.sh
+++ travis/before_script.sh
@@ -6,7 +6,9 @@ mkdir bin
 cp $IOS_SIM_DIR/bin/ios-sim ./bin/ios-sim
 
 gem install travis-artifacts
+TRAVIS_ARTIFACT_DIR=`pwd`/artifacts
+mkdir -p $TRAVIS_ARTIFACT_DIR
 
 rake ci:start-recording
 
-screencapture before_script.jpg
+screencapture $TRAVIS_ARTIFACT_DIR/before_script.jpg
