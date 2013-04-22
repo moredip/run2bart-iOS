e8936eabd274a0ed1f25672bcc0ea3733180954e
Pete Hodgson <git@thepete.net>
2013-04-22 16:52:52 -0700

    futzing with artifact dirs

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-screencapture $TRAVIS_ARTIFACT_DIR/after_script_a.jpg
+screencapture artifacts/after_script_a.jpg
 
 rake ci:stop-recording
 rake ci:save-recording["full-travis-run"]
 
 sleep 10
 
-mv /tmp/screenies $TRAVIS_ARTIFACT_DIR/screenies
+mv /tmp/screenies artifacts/screenies
 
-screencapture $TRAVIS_ARTIFACT_DIR/after_script_b.jpg
+screencapture artifacts/after_script_b.jpg
 
-travis-artifacts upload --path $TRAVIS_ARTIFACT_DIR --target-path $TRAVIS_BUILD_ID/$TRAVIS_JOB_ID
+travis-artifacts upload --path artifacts --target-path $TRAVIS_BUILD_ID/$TRAVIS_JOB_ID
--- travis/before_script.sh
+++ travis/before_script.sh
@@ -6,9 +6,8 @@ mkdir bin
 cp $IOS_SIM_DIR/bin/ios-sim ./bin/ios-sim
 
 gem install travis-artifacts
-export TRAVIS_ARTIFACT_DIR=`pwd`/artifacts
-mkdir -p $TRAVIS_ARTIFACT_DIR
+mkdir -p artifacts
 
 rake ci:start-recording
 
-screencapture $TRAVIS_ARTIFACT_DIR/before_script.jpg
+screencapture artifacts/before_script.jpg
