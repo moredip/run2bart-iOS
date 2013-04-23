296f281933a56cfcc4127240cda2393502dfd474
Pete Hodgson <git@thepete.net>
2013-04-22 17:18:40 -0700

    setting up Frank

--- travis/rake_tasks.rb
+++ travis/rake_tasks.rb
@@ -1,5 +1,22 @@
 require 'pathname'
-task :ci => ['unit-tests']
+
+ROOT_DIR = Pathname.new(__FILE__) + ".." + ".."
+ARTIFACTS_DIR = ROOT_DIR + "artifacts"
+PROJECT_DIR = ROOT_DIR + "Run2Bart"
+
+task :ci => ['clear-artifacts','ci:build','unit-tests']
 
 namespace :ci do
+  def move_into_artifacts( src )
+    FileUtils.mv( src, ARTIFACTS_DIR.to_s )
+  end
+
+  task "clear-artifacts" do
+    ARTIFACTS_DIR.rm_rf
+    ARTIFACTS_DIR.mkpath
+  end
+
+  task :build => ["xcode:sim-debug:cleanbuild"] do
+    move_into_artifacts( Pathname.glob(PROJECT_DIR+'build'+'Debug-iphonesimulator'+"*.app") )
+  end
 end
