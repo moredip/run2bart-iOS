7f0c3a85c1d4e3097d38b0f7997a1b8989350377
Pete Hodgson <git@thepete.net>
2013-04-27 14:22:50 -0700

    upload travis artifacts relative

--- travis/after_script.sh
+++ travis/after_script.sh
@@ -2,4 +2,4 @@
 
 screencapture $TRAVIS_ARTIFACTS_DIR/after_script.jpg
 
-travis-artifacts upload --path $TRAVIS_ARTIFACTS_DIR --target-path $TRAVIS_REPO_SLUG/$TRAVIS_BUILD_ID/$TRAVIS_JOB_ID
+travis-artifacts upload --root $TRAVIS_ARTIFACTS_DIR --path $TRAVIS_ARTIFACTS_DIR --target-path $TRAVIS_REPO_SLUG/$TRAVIS_BUILD_ID/$TRAVIS_JOB_ID
