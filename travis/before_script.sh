d1b3f6f6b16c239ad94588fde723087be58d75fd
Pete Hodgson <git@thepete.net>
2013-05-07 21:50:27 -0700

    brew update before brew install

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -4,6 +4,7 @@ env
 
 xcodebuild -list -workspace Run2Bart/Run2Bart.xcworkspace
 
+brew update
 brew install xctool ios-sim
 IOS_SIM_DIR=`brew info ios-sim | sed -n '3 p' | awk '{print $1}'`
 mkdir bin
