e696456d939abaaab4428db5e3010e68d6d59f93
Pete Hodgson <git@thepete.net>
2013-08-24 14:19:38 -0700

    Delete some specs that will be implemented by students

--- Run2Bart/Run2BartTests/UpcomingDeparturesSpec.m
+++ Run2Bart/Run2BartTests/UpcomingDeparturesSpec.m
@@ -9,17 +9,5 @@ describe(@"[UpcomingDeparture etdToDisplay]", ^{
                                                                                               etd:@(5)];
         [[[upcomingDeparture etdToDisplay] should] equal:@"5 mins"];
     });
-    
-    it(@"is correct for an etd of 1",^{
-        UpcomingDeparture *upcomingDeparture = [[UpcomingDeparture alloc] initWithDestinationName:@"blah"
-                                                                                              etd:@(1)];
-        [[[upcomingDeparture etdToDisplay] should] equal:@"1 min"];
-    });
-    
-    it(@"is correct for an etd of 0",^{
-        UpcomingDeparture *upcomingDeparture = [[UpcomingDeparture alloc] initWithDestinationName:@"blah"
-                                                                                              etd:@(0)];
-        [[[upcomingDeparture etdToDisplay] should] equal:@"now"];
-    });
 });
 SPEC_END
